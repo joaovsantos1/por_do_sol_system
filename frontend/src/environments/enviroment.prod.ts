f58e6aca6b5617587fb3a8ebec16a8975ec2d23b
Joao Victor <joao.vsantos1@outlook.com>
2026-09-15 14:24:21 -0300

    feat: change a link to API

--- frontend/src/environments/enviroment.prod.ts
+++ frontend/src/environments/enviroment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: "https://SEU-BACKEND.onrender.com/api",
-  hubUrl: "https://SEU-BACKEND.onrender.com/hubs",
+  apiUrl: "https://por-do-sol-system.onrender.com/api",
+  hubUrl: "https://por-do-sol-system.onrender.com/hubs",
 };
